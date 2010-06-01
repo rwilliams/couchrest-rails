1470df804204b67a1ed679ec8d6984edbb9989fb
Joe <joe.hohertz@rci.rogers.com>
2010-05-31 20:58:02 -0400

    provide has_attribute? to the document class, which rspec-rails seems to expect

--- lib/couch_rest_rails/document.rb
+++ lib/couch_rest_rails/document.rb
@@ -11,6 +11,12 @@ module CouchRestRails
     def self.unadorned_database_name
       database.name.sub(/^#{COUCHDB_CONFIG[:db_prefix]}/, '').sub(/#{COUCHDB_CONFIG[:db_suffix]}$/, '')
     end
+
+    # supplement the document class with methods that some rails extensions may expect
+    # due to an assumption the model is inherited from ActiveRecord::Base
+    def has_attribute?(attr_name)
+      self.has_key?(attr_name.to_s)
+    end
     
   end
 end
